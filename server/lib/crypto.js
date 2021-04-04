15691c4b9501cd111ce73a7e7564f50fbc935b50
joraavanes <jora_a@outlook.com>
2021-04-04 14:06:09 +0430

    pbkdf2 added to crypto lib - eslint new rules

--- server/lib/crypto.js
+++ server/lib/crypto.js
@@ -4,5 +4,10 @@ module.exports = {
     createHash: function(str, algorithm){
         const hash = crypto.createHmac(algorithm, process.env.HASH_SECRET_KEY);
         return hash.update(str).digest('hex');
+    },
+    PBKDF2: function(password, iterations, keylen, digestAlgorithm){
+        const salt = crypto.randomBytes(256).toString('hex');
+        const hashedPassword = crypto.pbkdf2Sync(password, salt, iterations, keylen, digestAlgorithm);
+        return hashedPassword.toString('hex');
     }
-}
+};
