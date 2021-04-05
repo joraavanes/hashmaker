1290d3ae6fcf0fae10fc06ff6624502995caf564
joraavanes <jora_a@outlook.com>
2021-04-05 12:32:44 +0430

    crypto function placed in trycatch block

--- server/lib/crypto.js
+++ server/lib/crypto.js
@@ -2,12 +2,20 @@ const crypto = require('crypto');
 
 module.exports = {
     createHash: function(str, algorithm){
-        const hash = crypto.createHmac(algorithm, process.env.HASH_SECRET_KEY);
-        return hash.update(str).digest('hex');
+        try {
+            const hash = crypto.createHmac(algorithm, process.env.HASH_SECRET_KEY);
+            return hash.update(str).digest('hex');
+        } catch (err) {
+            return err.message;
+        };
     },
     PBKDF2: function(password, iterations, keylen, digestAlgorithm){
-        const salt = crypto.randomBytes(256).toString('hex');
-        const hashedPassword = crypto.pbkdf2Sync(password, salt, iterations, keylen, digestAlgorithm);
-        return hashedPassword.toString('hex');
+        try {
+            const salt = crypto.randomBytes(256).toString('hex');
+            const hashedPassword = crypto.pbkdf2Sync(password, salt, iterations, keylen, digestAlgorithm);
+            return hashedPassword.toString('hex');
+        } catch (err) {
+            return err.message;
+        }
     }
 };
