33aac5c2c72087dd2702ec904c69ca5d58b224e3
joraavanes <jora_a@outlook.com>
2021-04-10 23:36:21 +0430

    Material icons added - Center align using makeStyles

--- hashmaker-client/src/components/Home.js
+++ hashmaker-client/src/components/Home.js
@@ -2,8 +2,23 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+import LockIcon from '@material-ui/icons/Lock';
+import BorderClearIcon from '@material-ui/icons/BorderClear';
+import TrackChangesIcon from '@material-ui/icons/TrackChanges';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    alignCenter: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center'
+    }
+}));
 
 const Home = () => {
+    const classes = useStyles();
+
     return (
         <div>
             <Typography 
@@ -15,14 +30,41 @@ const Home = () => {
                 Home
             </Typography>
 
-            <ButtonGroup>
+            <ButtonGroup className={classes.alignCenter}>
                 <Button type="button" color="secondary" variant="outlined">Home</Button>
-                <Button type="button" color="secondary" variant="outlined">Hash</Button>
-                <Button type="button" color="secondary" variant="outlined">PBKDF2</Button>
-                <Button type="button" color="secondary" variant="outlined">Encrypt</Button>
-                <Button type="button" color="secondary" variant="outlined">Decrypt</Button>
+                <Button 
+                    type="button" 
+                    color="secondary" 
+                    variant="outlined"
+                    startIcon={<BorderClearIcon fontSize="small" color="secondary"/>}
+                    >
+                    Hash
+                </Button>
+                <Button 
+                    type="button" 
+                    color="secondary" 
+                    variant="outlined"
+                    startIcon={<TrackChangesIcon fontSize="small" color="secondary"/>}>
+                    PBKDF2
+                </Button>
+                <Button 
+                    type="button" 
+                    color="secondary" 
+                    variant="outlined"
+                    startIcon={<LockIcon fontSize="small" color="secondary"/>}>
+                        Encrypt
+                </Button>
+                <Button 
+                    type="button" 
+                    color="secondary" 
+                    variant="outlined"
+                    startIcon={<LockOpenIcon fontSize="small" color="secondary"/>}>
+                        Decrypt
+                </Button>
 
             </ButtonGroup>
+            <div>
+            </div>
         </div>
     )
 }
