a4dfd0e71f8eb3aed96cd8ca5f67579dd5a8b34f
joraavanes <jora_a@outlook.com>
2021-04-24 15:22:39 +0430

    Add basic form for hashing

--- hashmaker-client/src/components/Hash.js
+++ hashmaker-client/src/components/Hash.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import { Grid, Paper, Typography, makeStyles, TextField, Button} from '@material-ui/core'
+
+const useStyles = makeStyles(styles => ({
+    smPadding:{
+        padding:'10px'
+    },
+    mdPadding:{
+        padding:'20px'
+    },
+    mt: {
+        marginTop: '20px'
+    },
+    colorWhite:{
+        color: '#fff'
+    }
+}));
 
 const Hash = () => {
+    const {mdPadding, mt, colorWhite} = useStyles();
+
     return (
-        <div>
-            <h2>Hash</h2>
-        </div>
+        <Grid container spacing={3} justify="center" className={mt}>
+            <Grid item xs={12} md={8} xl={12}>
+                <Paper className={mdPadding}>
+                    <Typography component="h2" variant="h5" className={mdPadding} align="center">
+                        Hash
+                    </Typography>
+                    <form noValidate autoComplete="off">
+                        <div>
+                            <TextField id="str" label="Plain text" fullWidth/>
+                        </div>
+                        <div>
+                            <TextField id="algorithm" label="algorithm" fullWidth/>
+                        </div>
+                        <div>
+                            <Button color="primary" variant="contained" className={`${mt} ${colorWhite}`}>Generate Hash</Button>
+                        </div>
+                    </form>
+                </Paper>
+            </Grid>
+        </Grid>
     )
 }
 
