59e976da7b24f4a07a5940e95ebe9b1068857056
joraavanes <jora_a@outlook.com>
2021-04-25 12:56:24 +0430

    Add input form for Pbkdf2 component

--- hashmaker-client/src/components/Pbkdf2.js
+++ hashmaker-client/src/components/Pbkdf2.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Grid, makeStyles, Paper, Typography } from '@material-ui/core'
+import React, { useState } from 'react'
+import { Grid, makeStyles, Paper, Typography, FormControl, TextField, InputLabel, Select, MenuItem, Button } from '@material-ui/core'
 
 const useStyles = makeStyles(styles => ({
     smPadding:{
@@ -20,11 +20,15 @@ const useStyles = makeStyles(styles => ({
 }));
 
 const Pbkdf2 = () => {
-    const {mt, smPadding, mdPadding} = useStyles();
+    const {mt, smPadding, mdPadding, selectMinWidth, colorWhite} = useStyles();
+
+    const [algorithm, setAlgorithm] = useState('SHA1');
+
+    const changeHandler = e => setAlgorithm(e.target.value);
 
     return (
         <Grid container spacing={3} justify="center" className={mt}>
-            <Grid item xs={12} sm={10}>
+            <Grid item xs={12} sm={10} xl={12}>
                 <Paper className={smPadding}>
                     <Typography
                         component="h2"
@@ -36,8 +40,42 @@ const Pbkdf2 = () => {
                         Password key derivation function 2 (PBKDF2)
                     </Typography>
                     <Grid container spacing={3}>
-                        <Grid item xs={12} sm={8}>Form</Grid>
-                        <Grid item xs={12} sm={4}>Output</Grid>
+                        <Grid item xs={12} sm={6}>
+                            <form autoComplete="off" noValidate>
+                                <div>
+                                    <TextField id="password" label="Plain Password" fullWidth/>
+                                </div>
+                                <div>
+                                    <TextField id="iterations" label="Iterations" fullWidth/>
+                                </div>
+                                <div>
+                                    <TextField id="keylen" label="Keylen" fullWidth/>
+                                </div>
+                                <div>
+                                    <FormControl className={mt}>
+                                        <InputLabel id="algorithm">Algorithm</InputLabel>
+                                        <Select
+                                            id="str"
+                                            labelId="algorithm"
+                                            label="algorithm"
+                                            fullWidth
+                                            className={selectMinWidth}
+                                            value={algorithm}
+                                            onChange={changeHandler}
+                                        >
+                                            <MenuItem value="SHA1">SHA1</MenuItem>
+                                            <MenuItem value="SHA256">SHA256</MenuItem>
+                                            <MenuItem value="SHA384">SHA384</MenuItem>
+                                            <MenuItem value="SHA512">SHA512</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                </div>
+                                <div>
+                                    <Button color="primary" variant="contained" className={`${mt} ${colorWhite}`}>Generate PBKDF2</Button>
+                                </div>
+                            </form>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>Output</Grid>
                     </Grid>
                 </Paper>
             </Grid>
