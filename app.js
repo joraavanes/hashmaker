026641fb21e4ffd2580c18e853a6562bb2e300cd
joraavanes <jora_a@outlook.com>
2021-05-02 02:20:18 +0430

    Add cors package and apply on server

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const cryptoAPI = require('./crypto/cryptoRoutes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/', cryptoAPI);
