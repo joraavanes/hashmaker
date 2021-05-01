cf55d831324b24331b4fe24a280d1347d9f394a1
joraavanes <jora_a@outlook.com>
2021-05-01 16:21:23 +0430

    Add form validation for Hash component

--- hashmaker-client/src/components/Hash.js
+++ hashmaker-client/src/components/Hash.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-import { Grid, Paper, Typography, makeStyles, TextField, Button, Select, MenuItem, FormControl, InputLabel} from '@material-ui/core'
+import { Grid, Paper, Typography, makeStyles, TextField, Button, Select, MenuItem, FormControl, InputLabel, jssPreset} from '@material-ui/core'
 
 const useStyles = makeStyles(styles => ({
     smPadding:{
@@ -26,33 +26,48 @@ const Hash = () => {
     const {mdPadding, mt, colorWhite, selectMinWidth} = useStyles();
 
     const {loading} = useSelector(cryptoSelector);
+    const [text, setText] = useState('');
     const [algorithm, setAlgorithm] = useState('SHA1');
 
-    const changehandler = e => setAlgorithm(e.target.value);
+    const textChangehanlder = e => setText(e.target.value);
+    const algorithmChangeHanlder = e => setAlgorithm(e.target.value);
+
+    const handleFormSubmit = e => {
+        e.preventDefault();
+
+        console.log(e.target.str.value);
+        console.log(algorithm);
+    };
 
     return (
         <Grid container spacing={3} justify="center" className={mt}>
             <Grid item xs={12} md={10} xl={12}>
                 <Paper className={mdPadding}>
                     <Typography component="h2" variant="h5" className={mdPadding} align="center">
-                        Hash - {loading.toString()}
+                        Hash
                     </Typography>
                     <Grid container spacing={3}>
                         <Grid item xs={12} sm={6}>
-                        <form noValidate autoComplete="off">
+                        <form noValidate autoComplete="off" onSubmit={handleFormSubmit}>
                             <div>
-                                <TextField id="str" label="Plain text" fullWidth/>
+                                <TextField 
+                                    id="str" 
+                                    label="Plain text" 
+                                    fullWidth
+                                    value={text}
+                                    onChange={textChangehanlder}
+                                />
                             </div>
                             <FormControl className={mt}>
-                                <InputLabel id="algorithm">Algorithm</InputLabel>
+                                <InputLabel htmlFor="algorithm">Algorithm</InputLabel>
                                 <Select
-                                    id="str"
+                                    id="algorithm"
                                     labelId="algorithm"
                                     label="algorithm"
                                     fullWidth
                                     className={selectMinWidth}
                                     value={algorithm}
-                                    onChange={changehandler}
+                                    onChange={algorithmChangeHanlder}
                                 >
                                     <MenuItem value="SHA1">SHA1</MenuItem>
                                     <MenuItem value="SHA256">SHA256</MenuItem>
@@ -62,7 +77,7 @@ const Hash = () => {
                                 {/* <TextField id="algorithm" label="algorithm" fullWidth/> */}
                             </FormControl>
                             <div>
-                                <Button color="primary" variant="contained" className={`${mt} ${colorWhite}`}>Generate Hash</Button>
+                                <Button type="submit" color="primary" variant="contained" className={`${mt} ${colorWhite}`}>Generate Hash</Button>
                             </div>
                         </form>
                         </Grid>
