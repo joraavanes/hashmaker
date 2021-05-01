d79b58fe759b9757a42125038c99827c163a45fb
joraavanes <jora_a@outlook.com>
2021-05-01 10:19:08 +0430

    Add cryptoActions

---
+++ hashmaker-client/src/state/actions/cryptoActions.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+export function createHMAC (plainText, algorithm){
+    return function(dispatch){
+        dispatch(toggleLoder(true));
+        
+        axios.post('http://localhost:4000/', {plainText, algorithm})
+            .then(res => {
+                console.log(res.data);
+                
+                dispatch(toggleLoder(false));
+            })
+            .catch(err=> {
+                
+                dispatch(toggleLoder(false));
+            });
+    }
+}
+
+export function toggleLoder(loading){
+    return {
+        type: 'LOADING',
+        loading
+    };
+}
