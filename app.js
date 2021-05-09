36a2195281cb5cf9bcfb7328bced75aaa56fe2e3
joraavanes <jora_a@outlook.com>
2021-05-09 23:45:02 +0430

    Add static file serving on server - Specify react app on root route of express app

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
-const cryptoAPI = require('./crypto/cryptoRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'hashmaker-client', 'build')));
 
-app.use('/', cryptoAPI);
+app.use('/', require('./crypto/cryptoRoutes'));
+
+app.use((req, res, next) => {
+    res.sendFile(path.resolve(__dirname, 'hashmaker-client', 'build', 'index.html'));
+});
 
 module.exports = app;
