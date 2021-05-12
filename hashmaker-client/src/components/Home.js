56e9e098d5813f9e0939b6880d8813c9e299e360
joraavanes <jora_a@outlook.com>
2021-05-12 21:14:12 +0430

    Add description to Home component

--- hashmaker-client/src/components/Home.js
+++ hashmaker-client/src/components/Home.js
@@ -36,8 +36,9 @@ const Home = () => {
                         Home
                     </Typography>
                     <Typography>
-                        Cryptography is and important part of any back-end structure. whether you are authorizing an incoming request to store or read data or giving authentication to a user.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis magnam quos reprehenderit temporibus quam nemo eligendi quasi praesentium mollitia, eaque, sunt cum modi provident accusantium doloribus fugit? Unde magnam obcaecati quia atque fuga natus iste sapiente, reprehenderit iure maxime excepturi eos ea quam earum dolorum eaque et, cupiditate nesciunt vitae sint. Libero voluptas vero vel quidem molestias excepturi. Quod optio voluptate quisquam dicta cumque, excepturi repellendus molestias rerum repudiandae! Nihil perferendis velit, iste, porro eligendi nemo facilis facere quod totam similique quos! Quidem amet voluptatibus reiciendis blanditiis culpa aliquid explicabo ullam aspernatur. Praesentium, tenetur laboriosam mollitia ad officiis quae voluptatem.
+                        Cryptography is an important part of any back-end structure. whether you are authorizing an incoming request to store or read data or giving authentication to a user.
+                        Nodejs at its core include a module called Crypto. There are plenty of hashing and cryptographical options ready to use. Althoough many may use 3rd party libraries such as bcrypt and aragon2, the Crypto module can make the most out of the job. 
+                        In this project we are gonna showcase number of important and commonly used ones which could be useful in many cases and the input and output of procedure as an visual HTML form, would help learn the concept better.
                     </Typography>
                     <Button color="primary" variant="contained" className={`${mt} ${colorWhite}`} component={NavLink} to={"/hash"}>Go for Hashing</Button>
                 </Paper>
